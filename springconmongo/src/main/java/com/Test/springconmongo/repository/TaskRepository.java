78c1ef1b67b01299129e2ff21613177d9a142c26
Zabdiel Julian <128274532+zquintero246@users.noreply.github.com>
2024-04-30 01:23:56 -0500

    Delete springconmongo directory

--- springconmongo/src/main/java/com/Test/springconmongo/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Test.springconmongo.repository;
-
-import com.Test.springconmongo.model.Task;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TaskRepository extends MongoRepository<Task, String> {
-}
-
